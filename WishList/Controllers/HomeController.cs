cebdab240bdec5234360aa4f5ed4f7e3a6518138
Garcia Hernandez <raimundo.garcia-hernandez@capgemini.com>
2022-07-27 12:17:29 +0200

    Completed module

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Home/Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("/Shared/Error");
+        }
+    }
+}

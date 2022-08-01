43c78f0fa0651f6bb691d37d1859b78a86cecb87
Garcia Hernandez <raimundo.garcia-hernandez@capgemini.com>
2022-08-01 10:28:19 +0200

    Completed module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context) { _context = context; }
+
+        public IActionResult Index(List<Item> container)
+        {
+            var model = _context.Items.ToList();
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
